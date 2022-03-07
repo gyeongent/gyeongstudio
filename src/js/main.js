edc643301ca1d14c836ca0d378d4d7d9133fc621
gyeongent <59150207+gyeongent@users.noreply.github.com>
2022-03-07 13:48:05 +0900

    add to features

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+function newsViewer(strValue)
+{
+	var newsLayer1 = document.getElementById('portfolio_contents_1');
+	var newsLayer2 = document.getElementById('portfolio_contents_2');
+
+	if (strValue == "1") {
+		newsLayer1.style.display = "block";
+		newsLayer2.style.display = "none";
+	}
+	else if (strValue == "2") {
+		newsLayer1.style.display = "none";
+		newsLayer2.style.display = "block";
+	}
+	else {
+		newsLayer1.style.display = "none";
+		newsLayer2.style.display = "none";
+	}
+}
+
+function soon() {
+  alert("Soon")
+}
