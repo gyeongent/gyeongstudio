61df49b561baef7291ca12f00fb439029e8521bb
gyeongent <59150207+gyeongent@users.noreply.github.com>
2022-03-07 14:56:14 +0900

    meta data

--- src/js/main.js
+++ src/js/main.js
@@ -20,3 +20,28 @@ function newsViewer(strValue)
 function soon() {
   alert("Soon")
 }
+
+var lang = "un"; //언어 값 받아올 변수. un은 undefined 의 앞 2글자.
+ 
+ if (navigator.language!=null) //이 값이 null이 아니면. 즉 크롬이나 파폭이면
+ {
+     lang = navigator.language;
+ } else if (navigator.userLanguage!=null) { //이 값이 null이 아니면. 즉 익스라면
+     lang = navigator.userLanguage;
+ } else if (navigator.systemLanguage!=null) { //여기까지는 안 올거 같은데 혹시나 해서
+     lang = navigator.systemLanguage;
+ } else { //이도저도 아니면
+     lang="un";
+ }
+ 
+ lang = lang.toLowerCase(); //받아온 값을 소문자로 변경
+ lang = lang.substring(0, 2); //소문자로 변경한 갚의 앞 2글자만 받아오기
+ 
+  if (lang=="ko") { //한국어라면
+      setLanguage(ko);
+  } else if (lang=="cn" || lang=="tw" || lang=="zh") { //중국어라면
+      alert("중국어");
+  } else { //그 외~
+      alert("그 외");
+  }
+ 
