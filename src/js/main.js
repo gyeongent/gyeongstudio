dbe6d779d79d09f052934d0931efa4b19106685d
gyeongent <59150207+gyeongent@users.noreply.github.com>
2022-03-08 11:26:40 +0900

    auto set language

--- src/js/main.js
+++ src/js/main.js
@@ -21,4 +21,18 @@ function soon() {
   alert("Soon")
 }
 
+function setCookie(key, value, expiredays) {
+    let todayDate = new Date();
+    todayDate.setDate(todayDate.getDate() + expiredays); // 현재 시각 + 일 단위로 쿠키 만료 날짜 변경
+    //todayDate.setTime(todayDate.getTime() + (expiredays * 24 * 60 * 60 * 1000)); // 밀리세컨드 단위로 쿠키 만료 날짜 변경
+    document.cookie = key + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";";
+}
 
+function getLang(){
+	var userLang = navigator.language || navigator.userLanguage;
+	var strLang = userLang.substring(0,2)
+	return strLang;
+}
+
+console.log('Detected Language : ' + getLang());
+console.log('Set Language => ' + getLang());
