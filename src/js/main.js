350eda1b7dfb1c6d80f52b7c6ac72e22178a2575
gyeongent <59150207+gyeongent@users.noreply.github.com>
2022-03-23 09:32:33 +0900

    smoothwheel

--- src/js/main.js
+++ src/js/main.js
@@ -62,3 +62,35 @@ if (getLang() == "uk") {
 if (window.location == "https://gyeong.net/news/") {
 	window.location.href = "https://gyeong.net/news"
 }
+
+function isSameAsLocation(uriString) {
+	const uri = new URL(uriString);
+
+	return (
+		uri.origin === window.location.origin &&
+		uri.pathname ===window.location.pathname
+	);
+}
+
+function pageTransition(nodeList) {
+	nodeList.forEach((node) => {
+		if (!(node instanceof HTMLAnchorElement)) return;
+
+		const { href } = node;
+
+		if (!href || node.target === "_blank" || isSameAsLocation(href)) return;
+
+		node.addEventListener("click", (event) => {
+			event.preventDefault();
+
+			document.body.addEventListener(
+				"transitionend",
+				() => {
+					window.location.href = href;
+				},
+				{passive: true, once: true}
+			);
+			document.body.classList.add("hidden");
+		})
+	})
+}
